8b0024f7768305177b2e83f7bc5302cc59ce622e
Eric Butera <eric@ericbutera.com>
2026-09-13 07:05:08 -0400

    fix: expose worker poll spans

--- rust/kaleido/src/background_jobs/worker/task_worker.rs
+++ rust/kaleido/src/background_jobs/worker/task_worker.rs
@@ -104,7 +104,7 @@ impl TaskWorker {
     }
 
     async fn process_batch(&self) -> Result<usize, WorkerError> {
-        let batch_span = tracing::debug_span!(
+        let batch_span = tracing::info_span!(
             "background_task.poll",
             task.batch_size = self.batch_size,
             task.count = tracing::field::Empty,
