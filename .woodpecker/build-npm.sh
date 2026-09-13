c47e065fd24a755e31eb6cc154ee4c5176e3a6c6
Eric Butera <eric@ericbutera.com>
2026-09-13 07:51:20 -0400

    ci: remove woodpecker release
    crates & npm support OIDC from github, but not external systems (or, not easily).

--- .woodpecker/build-npm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-npm install -g pnpm@9
-cd typescript/packages/kaleido
-pnpm install --no-frozen-lockfile
-pnpm typecheck
-pnpm build
--- .woodpecker/package-rust.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-cd rust
-cargo fmt --all --check
-cargo package -p kaleido --locked
